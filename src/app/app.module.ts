cdce63f3b16659a768cea841f936bd21ffb9e615
vinicius.oliveira <vinicius.oliveira@movemais.com>
2024-05-31 16:34:08 -0300

    'bkp-mm'

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,25 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { ListaModule } from './lista/lista.module'
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+//import { ListViewComponent } from './list-view/list-view.component';
+//import { InputComponent } from './lista/input/input.component';
 
 @NgModule({
-  declarations: [
-    AppComponent
+  declarations: [ // componentes, diretivas e pipes
+    AppComponent //, ListViewComponent
   ],
-  imports: [
+  imports: [ // modulos
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    ListaModule,
   ],
-  providers: [],
-  bootstrap: [AppComponent]
+  exports: [ // componentes, diretivas e pipes de poderão ser importadas externamente
+
+  ],
+  providers: [], // injeções de dependências e services
+  bootstrap: [AppComponent] // componentes que serão automaticamente inicializados (como o método main)
 })
 export class AppModule { }
